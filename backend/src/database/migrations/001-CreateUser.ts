3759410f2542af7588a6c2aa96c895be73789849
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-12-06 19:59:52 -0300

    ✅ add jest config

--- backend/src/database/migrations/001-CreateUser.ts
+++ backend/src/database/migrations/001-CreateUser.ts
@@ -1,20 +1,20 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUser1638712731708 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "users",
+        name: 'users',
         columns: [
-          { name: "id", type: "varchar", isPrimary: true },
-          { name: "password", type: "varchar" },
-          { name: "email", type: "varchar", isUnique: true },
-          { name: "username", type: "varchar", isUnique: true },
-          { name: "created_at", type: "timestamp", default: "now()" },
+          { name: 'id', type: 'varchar', isPrimary: true },
+          { name: 'password', type: 'varchar' },
+          { name: 'email', type: 'varchar', isUnique: true },
+          { name: 'username', type: 'varchar', isUnique: true },
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
           {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()",
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
             isNullable: false,
           },
         ],
@@ -23,6 +23,6 @@ export class CreateUser1638712731708 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("users");
+    await queryRunner.dropTable('users');
   }
 }
--- backend/src/database/migrations/003-CreateReading.ts
+++ backend/src/database/migrations/003-CreateReading.ts
@@ -3,25 +3,25 @@ import {
   QueryRunner,
   Table,
   TableForeignKey,
-} from "typeorm";
+} from 'typeorm';
 
 export class CreateReading1638717641779 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "reading",
+        name: 'reading',
         columns: [
-          { name: "id", type: "varchar", isPrimary: true },
-          { name: "userId", type: "varchar" },
-          { name: "literaryWorkId", type: "varchar" },
-          { name: "current_chapter", type: "int" },
-          { name: "url_to_current_chapter", type: "varchar", isUnique: true },
-          { name: "status", type: "varchar" },
-          { name: "created_at", type: "timestamp", default: "now()" },
+          { name: 'id', type: 'varchar', isPrimary: true },
+          { name: 'userId', type: 'varchar' },
+          { name: 'literaryWorkId', type: 'varchar' },
+          { name: 'current_chapter', type: 'int' },
+          { name: 'url_to_current_chapter', type: 'varchar', isUnique: true },
+          { name: 'status', type: 'varchar' },
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
           {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()",
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
             isNullable: false,
           },
         ],
@@ -29,27 +29,27 @@ export class CreateReading1638717641779 implements MigrationInterface {
     );
 
     await queryRunner.createForeignKey(
-      "reading",
+      'reading',
       new TableForeignKey({
-        columnNames: ["userId"],
-        referencedColumnNames: ["id"],
-        referencedTableName: "users",
-        onDelete: "CASCADE",
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
       })
     );
 
     await queryRunner.createForeignKey(
-      "reading",
+      'reading',
       new TableForeignKey({
-        columnNames: ["literaryWorkId"],
-        referencedColumnNames: ["id"],
-        referencedTableName: "literary_work",
-        onDelete: "CASCADE",
+        columnNames: ['literaryWorkId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'literary_work',
+        onDelete: 'CASCADE',
       })
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("reading");
+    await queryRunner.dropTable('reading');
   }
 }
